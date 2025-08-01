dd608d3011a5f3e60796c66d4b2fdf147b099b3f
marianalinda123 <140506972+marianalinda123@users.noreply.github.com>
2025-08-01 13:46:05 +0000

    aula 01/08

---
+++ main.js
@@ -0,0 +1,18 @@
+const numeroSenha= document.querySelector('.parametro-senha_texto');
+let tamanhoSenha=12;
+numeroSenha.textContent=tamanhoSenha;
+const botoes=document.querySelectorAll('.parametro-senha_botao');
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+function diminuiTamanho(){
+    if(tamanhoSenha > 1){
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    if(tamanhoSenha < 20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
